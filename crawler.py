9db826576ca2beb2abbc90ed38d1a633ae2ca912
David Herren <herdav.mobile@gmail.com>
2018-05-11 15:33:14 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 # CRAWLER
 
 from urllib.request import urlopen
+from datetime import datetime as dt
 import socket
 
 HOST = 'localhost'
@@ -8,10 +9,17 @@ PORT = 5204
 ADDR = (HOST, PORT)
 BUFSIZE = 4096
 
-url_gas = ' '
-id_gas_current = ' '
-url_wheat = ' '
-id_wheat_current = ' '
+url_gas = ''
+id_gas_last_trade = ''
+id_gas_52w_high = ''
+id_gas_52w_low = ''
+
+url_wheat = ''
+id_wheat_last_trade = ''
+id_wheat_52w_high = ''
+id_wheat_52w_low = ''
+
+count = 0
 
 
 def crawler(url, id_value):
@@ -27,24 +35,39 @@ def crawler(url, id_value):
         if site_read[i] == '<':
             pos_end = i
             break
-
     value = site_read[pos_start:pos_end]
-    value = round(float(value.replace(',', '.')), 2)
+    value = round(float(value.replace(',', '.')), 3)
     return value
 
 
-j = 0
-while j < 20:
-    gas_current_price = crawler(url_gas, id_gas_current)
-    print('GAS:  ', gas_current_price, 'USD/Btu')
-    wheat_current_price = crawler(url_wheat, id_wheat_current)
-    print('WHEAT:', wheat_current_price, 'USD/bu')
-    j += 1
+def conv(float_value):
+    conversation = str(int(1000 * float_value))
+    return conversation
 
-    data = ('a' + str(int(100 * gas_current_price)) + 'b' + str(int(100 * wheat_current_price)) + '#')
-    print('Port Stream: ' + data)
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(ADDR)
-    data = data.encode()
-    client.send(data)
-    client.close()
+
+while True:
+    dt.now()
+    if dt.now().second % 10 == 0:
+        gas_current_price = crawler(url_gas, id_gas_last_trade)
+        gas_52high_price = crawler(url_gas, id_gas_52w_high)
+        gas_52low_price = crawler(url_gas, id_gas_52w_low)
+        data_gas = ('aa' + conv(gas_current_price) + 'ab' + conv(gas_52high_price) + 'ac' + conv(gas_52low_price))
+
+        wheat_current_price = crawler(url_wheat, id_wheat_last_trade)
+        wheat_52high_price = crawler(url_wheat, id_wheat_52w_high)
+        wheat_52low_price = crawler(url_wheat, id_wheat_52w_low)
+        data_wheat = ('ba' + conv(wheat_current_price) + 'bb' + conv(wheat_52high_price) + 'bc' + conv(wheat_52low_price))
+
+        data = ("Crawler: " + data_gas + data_wheat + '#')
+
+        print('[' + str(count) + '] Port Stream: ' + data)
+        print('GAS:  ', gas_current_price, gas_52high_price, gas_52low_price, 'mmGBP/Btu')
+        print('WHEAT:', wheat_current_price, wheat_52high_price, wheat_52low_price, 'USD/bu')
+
+        count += 1
+
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(ADDR)
+        data = data.encode()
+        client.send(data)
+        client.close()
